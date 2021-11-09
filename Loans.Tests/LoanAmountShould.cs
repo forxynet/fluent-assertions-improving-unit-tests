f0b20751b4bd965633193c1d55c6e282a7a44d7d
Hakan Baştürk <forxynet@gmail.com>
2021-11-09 14:08:02 +0300

    first commit

---
+++ Loans.Tests/LoanAmountShould.cs
@@ -0,0 +1,23 @@
+﻿using Loans.Domain.Applications.Values;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Loans.Tests
+{
+    public class LoanAmountShould
+    {
+        [Test]
+        public void StoreCurrencyCode()
+        {
+            var sut = new LoanAmount("USD", 100_000);
+
+            //Assert.That(sut.CurrencyCode, Is.EqualTo("USD"));
+            sut.CurrencyCode.Should().Be("USD");
+            sut.CurrencyCode.Should().Contain("U");
+            sut.CurrencyCode.Should().Match("*D");
+            sut.CurrencyCode.Should().BeOneOf("USD", "AUD", "GBP");
+                    
+
+        }
+    }
+}
---
+++ Loans.Tests/ProductComparerShould.cs
@@ -0,0 +1,112 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Loans.Domain.Applications;
+using Loans.Domain.Applications.Values;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Loans.Tests
+{
+    [ProductComparison]
+    public class ProductComparerShould
+    {
+        private List<LoanProduct> products;
+        private ProductComparer sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // Simulate long setup init time for this list of products
+            // We assume that this list will not be modified by any tests
+            // as this will potentially break other tests (i.e. break test isolation)
+            products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+        }
+
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+        }
+
+
+        [Test]        
+        public void ReturnCorrectNumberOfComparisons()
+        {
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //Assert.That(comparisons, Has.Exactly(3).Items);
+
+            comparisons.Should().HaveCount(3);
+        }
+
+
+        [Test]
+        public void NotReturnDuplicateComparisons()
+        {
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //Assert.That(comparisons, Is.Unique);
+            comparisons.Should().OnlyHaveUniqueItems();
+        }
+
+
+        [Test]
+        public void ReturnComparisonForFirstProduct()
+        {
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            // Need to also know the expected monthly repayment
+            var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+
+            //Assert.That(comparisons, Does.Contain(expectedProduct));
+
+            comparisons.Should().Contain(expectedProduct);
+        }
+
+        [Test]
+        public void ReturnNonNullEmptyComparision() {
+            List<MonthlyRepaymentComparison> comparison = sut.CompareMonthlyRepayments(new LoanTerm(30));
+            //comparison = null;
+            comparison.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void ReturnComparionsSortedByProductName() {
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            comparisons.Should().BeInAscendingOrder(x => x.ProductName);
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct_WithPartialKnownExpectedValues()
+        {
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons,
+                        Has.Exactly(1)
+                           .Matches(new MonthlyRepaymentGreaterThanZeroConstraint("a", 1)));
+        }
+
+        [Test]
+        public void AssertionScopes() {
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+            using (new AssertionScope()) {
+                comparisons.Should().NotBeNullOrEmpty();
+                comparisons.Should().HaveCount(3); //fail - should be 3
+                comparisons.Should().OnlyHaveUniqueItems();
+                comparisons.Should().Contain(new MonthlyRepaymentComparison("a", 1, 643.28m));
+                comparisons.Should().BeInAscendingOrder(x=>x.ProductName);
+            }
+        }
+    }
+}
